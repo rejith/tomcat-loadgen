6ceb32bed36c42256428bf8d3be301fc45d2593f
Rejith Krishnan <rejith@usa.net>
2017-11-10 20:51:54 -0500

    Update HelloWorldServlet.java

--- src/com/HelloWorldServlet.java
+++ src/com/HelloWorldServlet.java
@@ -57,7 +57,7 @@ public class HelloWorldServlet extends HttpServlet {
 	}
 
 	public void init() throws ServletException {
-		threadFunc();
+		threadFunc2();
 	}
 
 	/**
@@ -67,7 +67,7 @@ public class HelloWorldServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		
-		response.setContentType("text/html");
+		response.setContentType2("text/html");
 		PrintWriter printWriter = response.getWriter();
 		printWriter.println("HelloWorld Testing Code");
 
